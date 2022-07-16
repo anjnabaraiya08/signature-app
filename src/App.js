4b03c31e710ff1fe72529349c9d0fb4b4ebf72fb
anjnabaraiya08 <anjlibaraiya08@gmail.com>
2022-07-16 11:26:08 +0530

    signature app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import SignaturePad from 'react-signature-canvas';
 import './App.css';
 
 function App() {
+
+  const [trimmedURL, setTrimmedURL] = useState([]);
+  const signatureRef = useRef({});
+
+  function trim() {
+    setTrimmedURL([...trimmedURL, signatureRef.current.toDataURL("image/png")]);
+    signatureRef.current.clear();
+  }
+
+  function clear() {
+    signatureRef.current.clear();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Signature Pad</h1>
+      <div className='signaturePad'>
+        <SignaturePad ref={signatureRef} height={150} width={300} penColor={'#0dd2ff'} />
+        <button className='save_button' onClick={trim} >Save</button>
+        <button className='clear_button' onClick={clear}>Clear</button>
+      </div>
+      <div className='image-section'>
+        {trimmedURL.length ? trimmedURL.map((url, i) => (
+            <img key={i} alt={'signature'} src={url} className='signImage' />
+        )) : null}
+      </div>
     </div>
   );
 }
